96d20de137a4ef129d9a3c01d96806a62bbc07b5
Ali-Rahimpoor <ali.rahimpoor.ar@gmail.com>
2025-05-04 21:52:09 +0330

    feat: add ReactModal & click image and download

--- src/components/NASAGallery.jsx
+++ src/components/NASAGallery.jsx
@@ -2,15 +2,20 @@ import { useCallback, useContext, useEffect, useState } from "react";
 import { Get_APOD, Get_EARTH, Get_MARS } from "../services/NasaApi";
 import { Context } from "../services/Context";
 import Loading from "./Loading";
+import ReactModal from "react-modal";
+import { FaDownload } from "react-icons/fa6";
+import { IoClose } from "react-icons/io5";
 const NASAGallery = ()=>{
    const {title} = useContext(Context);
    const [loading,setLoading] = useState(false);
    const [images,setImages] = useState([]);
+   const [selectedImage,setSelectedImage] = useState(null);
+   const [isModalOpen,setIsModalOpen] = useState(false);
    useEffect(()=>{
       const fetchAPODImages =  async ()=>{
         try{
             setLoading(true);
-            const {data} = await Get_APOD();
+            const {data} = await Get_APOD(3);
             console.log(data)
             setImages(data);
         }catch(err){
@@ -62,13 +67,35 @@ const NASAGallery = ()=>{
       }
    },[title])
 
+   const handleImageClick = (img)=>{
+      setSelectedImage(img);
+      setIsModalOpen(true);
+   }
+   const handleDownload = ()=>{
+      if (!selectedImage) return;
+
+      window.open(selectedImage.hdurl || selectedImage.url, '_blank');
+
+      // const imageUrl = selectedImage.hdurl || selectedImage.url;
+      // const link = document.createElement('a');
+      // link.href = imageUrl;
+      // link.download = `nasa-${selectedImage.date || 'image'}.jpg`;
+      // document.body.appendChild(link);
+      // link.click();
+      // document.body.removeChild(link);
+   }
+   const closeModal = ()=>{
+      setIsModalOpen(false)
+      setSelectedImage(null);
+   }
+
    if(loading) return <Loading/>
-   if (images.length === 0) return <div>No images found</div>;
+   if (!images||images.length === 0) return <div>No images found</div>;
    return(
       <>
          <section className="container font-Karla mt-5 p-2 gap-8 grid grid-cols-3">
             {images&&images.map((img,index)=>(
-               <div key={index} className="relative hover:scale-95 transition-transform cursor-pointer">
+               <div onClick={()=>handleImageClick(img)} key={index} className="relative hover:scale-95 transition-transform cursor-pointer">
                   <img className="w-full h-full rounded object-cover"  src={img.hdurl || img.url} alt="Photo" />
                   <div className="text-gray-200">
                   <p className="absolute line-clamp-1 shadow bg-black/20 p-1 text-sm top-3 left-3">{img.title}</p>
@@ -77,7 +104,35 @@ const NASAGallery = ()=>{
                </div>
             ))}
          </section>
-
+            <ReactModal
+            isOpen={isModalOpen}
+            onRequestClose={closeModal}
+            className="modal"
+            overlayClassName='overlay'
+            contentLabel="Image Modal"
+            >
+               {selectedImage &&(
+                   <div className="relative w-[700px] h-[700px] "
+                 
+                   >
+                   <img 
+                      src={selectedImage.hdurl || selectedImage.url} 
+                      alt={"PHOTO"} 
+                      className="object-cover w-full h-full rounded-sm"
+                   />
+                   <div className="flex items-center gap-10 text-zinc-900 justify-around left-5 absolute top-5 bg-white/30 p-3 rounded">
+                   <button onClick={closeModal} className="cursor-pointer">
+                        <IoClose className="w-14 h-14 rounded-xl p-1" />
+                   </button>
+                   <p className="text-2xl">{selectedImage.copyright}</p>
+                   <button onClick={handleDownload} className="cursor-pointer">
+                     <FaDownload className="w-14 h-14 rounded-xl p-2" />
+                   </button>
+                   </div>
+                   <p className="absolute bottom-0 text-zinc-800 bg-white/40 px-6 line-clamp-3">{selectedImage?.explanation}</p>
+                </div>
+               )}
+            </ReactModal>
       </>
    )
 }
