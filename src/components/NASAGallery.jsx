4375eea66b14c2932c60896a4525a688b0d549f3
Ali-Rahimpoor <ali.rahimpoor.ar@gmail.com>
2025-05-07 09:05:50 +0330

    Responsive APOD & MARS

--- src/components/NASAGallery.jsx
+++ src/components/NASAGallery.jsx
@@ -39,13 +39,13 @@ const NASAGallery = ()=>{
       <>
       {title ==='APOD'?(
 
-         <section className="container font-Karla mt-2 p-2 gap-8 grid grid-cols-3">
+         <section className="container font-Karla mt-2 p-2 md:gap-8 xs:gap-7 gap-4 grid grid-cols-2 sm:grid-cols-3">
             {images&&images.map((img,index)=>(
                <div onClick={()=>handleImageClick_APOD(img)} key={index} className="relative hover:scale-95 transition-transform cursor-pointer">
-                  <img className="w-full h-full rounded object-cover"  src={img.hdurl || img.url} alt="Photo" />
+                  <img className="w-full h-full rounded-md object-cover"  src={img.hdurl || img.url} alt="Photo" />
                   <div className="text-gray-200">
-                  <p className="absolute line-clamp-1 shadow bg-black/20 p-1 text-sm top-3 left-3">{img.title}</p>
-                  <p className="absolute bottom-2 line-clamp-1 right-3 bg-black/20 shadow p-1 font-KarlaBold tracking-wide">{img.date}</p>
+                  <p className="absolute line-clamp-1 shadow md:text-sm  bg-black/20 p-1 text-[10px] xs:text-xs md:top-3 top-1 left-1 md:left-3">{img.title}</p>
+                  <p className="absolute md:bottom-2 bottom-1 text-[9px] xs:text-xs md:text-base line-clamp-1 right-1 md:right-2 bg-black/20 shadow p-1 font-KarlaBold tracking-wide">{img.date}</p>
                   </div>
                </div>
             ))}
@@ -53,13 +53,13 @@ const NASAGallery = ()=>{
 
          ):(
 
-         <section className="container font-Karla mt-2 p-2 gap-8 grid grid-cols-3">
+         <section className="container font-Karla mt-2 p-2 md:gap-8 xs:gap-7 gap-4 grid grid-cols-2 sm:grid-cols-3">
                   {images&&images?.map((img,index)=>(
                      <div onDoubleClick={()=>{handleImageClick_MARS(img)}} key={index} className="relative cursor-pointer">
-                        <img src={img.img_src} className="w-full h-full rounded object-cover" alt="Photo" />
+                        <img src={img.img_src} className="w-full h-full rounded-md object-cover" alt="Photo" />
                         <div>
-                           <p className="absolute top-3 left-3 bg-black/20 text-gray-200 rounded-sm px-2 py-1">{img?.earth_date || "NODate"}</p>
-                           <p className="absolute bottom-2 p-1 rounded text-xs bg-black/20  right-2 text-gray-200">{img?.camera?.name || "NoName"}</p>
+                           <p className="absolute sm:top-3 top-1 left-1 sm:left-3 bg-black/20 text-xs sm:text-base text-gray-200 rounded-sm sm:px-2 px-1 py-1">{img?.earth_date || "NODate"}</p>
+                           <p className="absolute xs:bottom-2 bottom-1 p-1 rounded text-[9px] sm:text-xs bg-black/20 right-1 xs:right-2 text-gray-200">{img?.camera?.name || "NoName"}</p>
                         </div>
                      </div>
                   ))}
@@ -67,12 +67,12 @@ const NASAGallery = ()=>{
 
       )}
 
-      <nav className="container flex items-center justify-center bg-gradient-to-tl from-zinc-900 to-zinc-600 p-2">
+      <nav className="flex items-center justify-center bg-gradient-to-tl from-zinc-900 to-zinc-600 p-2">
          <button onClick={handleRefresh} disabled={loading} className="shadow text-xl font-Karla bg-white/5 text-gray-200 px-8 cursor-pointer py-2 rounded">
          {loading ? (
           <span className="animate-spin">↻</span>
         ) : (
-          <span>↻ Refresh (Skip Cache)</span>
+          <span className="xs:text-base text-sm">↻ Refresh (Skip Cache)</span>
         )}
          </button>
       </nav>
