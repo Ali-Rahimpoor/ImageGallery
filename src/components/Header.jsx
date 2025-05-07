922a613cece690a85ed99c8c30a16c43979518ec
Ali-Rahimpoor <ali.rahimpoor.ar@gmail.com>
2025-05-07 08:50:17 +0330

    Responsive Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,23 +17,23 @@ const Header = ()=>{
    const {setTitle} = useContext(Context);
    return(
       <header className="relative font-Karla bg-black w-full text-gray-200">
-         <h1 className="font-Londrina text-gray-300 text-center text-7xl border-b border-gray-600 p-10">
+         <h1 className="font-Londrina text-gray-300 text-center text-3xl sm:text-7xl border-b border-gray-600 p-10">
             Image Gallery
          </h1>
          {/* Header content */}
-         <section className="flex mt-10 mx-auto w-[1200px]  justify-between items-center">  
-            <div className="flex items-start justify-between gap-x-2">  
+         <section className="flex flex-col-reverse mb-10 xl:mb-0  xl:mt-10 mx-auto xl:flex-row xl:w-[1200px]  justify-between items-center">  
+            <div className="flex xl:items-start md:items-center  justify-between gap-x-2">  
 
-               <img className="w-75 h-fit rounded-full" loading="lazy" src={HeaderImg3_url} alt="Photo" />
+               <img className="xl:w-75 md:w-40 w-30 h-fit rounded-full" loading="lazy" src={HeaderImg3_url} alt="Photo" />
 
                <div>
-                  <h3 className="text-6xl ">The sky begins here 
+                  <h3 className="xl:text-6xl sm:text-3xl text-lg xs:text-2xl font-KarlaBold ">The sky begins here 
                   </h3>
-                  <p className="text-xl mt-2 pl-2">
+                  <p className="xl:text-xl md:block hidden md:text-lg mt-2 pl-2">
                   Images from space, for moments of silence and wonder
                   </p>
-                  <p className="text-xl pl-2">Look. Enjoy</p>
-                  <p className="text-xl pl-2"> Welcome to the galaxy</p>
+                  <p className="xl:text-xl xs:text-lg  pl-2">Look. Enjoy</p>
+                  <p className="xl:text-xl xs:text-lg text-sm pl-2"> Welcome to the galaxy</p>
                </div>
            </div>
            <Swiper
@@ -44,21 +44,21 @@ const Header = ()=>{
               disableOnInteraction: false,
             }}            
             modules={[Autoplay]}
-            className="w-[200px] h-[200px] cursor-help rounded-full overflow-hidden border border-gray-600"
+            className="xs:w-[200px] xs:h-[200px] w-[140px] h-[140px] cursor-help my-5 xl:my-0 rounded-full overflow-hidden border border-gray-600"
            >
-             <SwiperSlide className="pt-10 pl-10">
+             <SwiperSlide className="xs:pt-10 pt-5 pl-5 xs:pl-10">
              <img loading="lazy" className="w-25" src={Gif3_url} alt="" />
              </SwiperSlide>
-             <SwiperSlide className="pt-10 pl-5">
+             <SwiperSlide className="xs:pt-10 pt-5 pl-2 xs:pl-5">
                 <img loading="lazy" className="w-30" src={Gif4_url} alt="" />
              </SwiperSlide>
-             <SwiperSlide className="pt-15 pl-8">
+             <SwiperSlide className="xs:pt-15 pt-9 pl-2 xs:pl-8">
                 <img loading="lazy" src={Gif2_url} alt="" />
              </SwiperSlide>
            </Swiper>
          </section>
          {/* Nav for select Title */}
-         <nav className="w-[400px] font-Roboto text-2xl mx-auto ">
+         <nav className="xs:w-[400px] w-screen px-2 sm:px-0 font-Roboto text-2xl mx-auto ">
             <h2 className="text-center border-b mb-2 ">Choose a Title</h2>
             <ul className="flex justify-between items-center ">
                <li className="bg-indigo-700 hover:pb-8 transition-all p-3 font-RobotoBold tracking-wider text-lg rounded-md rounded-b-none">
@@ -76,7 +76,7 @@ const Header = ()=>{
          <figure className="">
             {/* Logo */}
             <Link to='/home'>
-               <img className="fixed z-20 w-25 cursor-pointer left-4 top-4" src={Gif1_url} alt="Logo" />
+               <img className="fixed z-30 xs:w-25 w-15 cursor-pointer left-4 top-4" src={Gif1_url} alt="Logo" />
             </Link>
          </figure>
       </header>
