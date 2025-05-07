7d90f23bdc9f90c2ec8a16c8b069ace6b869d894
Ali-Rahimpoor <ali.rahimpoor.ar@gmail.com>
2025-05-07 12:38:07 +0330

    finall changes for V1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,9 +3,12 @@ import Space_svg from '../assets/svg/space.svg?react'
 const Home = ()=>{
 
    return(
+      <>
       <div className='flex items-center justify-center'>
          <Space_svg className='size-72' />
       </div>
+      <h2 className='text-center font-Karla tracking-tight'>Developed by Ali Rahimpoor</h2>
+      </>
    )
 }
 export default Home;
